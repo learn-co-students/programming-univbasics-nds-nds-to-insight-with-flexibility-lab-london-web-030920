98dc798529dc8a7778ce2c38f9bf6e0c7b11adfa
George Cooper <coopsgeorge@gmail.com>
2020-03-06 16:37:52 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
+require 'pry'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
@@ -21,7 +22,7 @@ def flatten_a_o_a(aoa)
 end
 
 def movie_with_director_name(director_name, movie_data)
-  { 
+  {
     :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
@@ -48,6 +49,14 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  array = []
+  movie_index = 0
+while movie_index < movies_collection.length
+  new_movie_data = movie_with_director_name(name, movies_collection[movie_index])
+  array.push(new_movie_data)
+  movie_index += 1
+end
+  array
 end
 
 
@@ -63,6 +72,22 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+studio_value_hash = {}
+movie_index = 0
+
+while movie_index < collection.length
+  movie_studio = collection[movie_index][:studio]
+  movie_gross = collection[movie_index][:worldwide_gross]
+
+    if studio_value_hash[movie_studio]
+    studio_value_hash[movie_studio] += movie_gross
+
+  else
+    studio_value_hash[movie_studio] = movie_gross
+  end
+  movie_index += 1
+end
+studio_value_hash
 end
 
 def movies_with_directors_set(source)
@@ -76,7 +101,34 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  # movies_with_director_array = [ movies: ["At the park"], ["On the couch"]}, ]
+array = []
+director_index = 0
+
+while director_index < source.length
+  director_name_new = source[director_index][:name]
+  director_movies = source[director_index][:movies]
+
+  array.push(movies_with_director_key(director_name_new, director_movies))
+
+  # while movie_index < source[director_index].length
+  #   director_name_new = source[director_index][:name]
+  #   individual_movie_data = source[director_index][:movies][movie_index][:title]
+  #
+  #   array.push({director_name_new => individual_movie_data})
+  #
+  #   movie_index += 1
+  # end
+  director_index += 1
 end
+
+
+
+array
+
+end
+
+
 
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
